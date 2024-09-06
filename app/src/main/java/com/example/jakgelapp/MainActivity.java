a2a3f34db6a55fc5fd5a0aec342653516133bd61
AK7563 <nailzhavieradhyaksa441@gmail.com>
2024-09-06 19:57:25 +0700

    Writing Feature

--- app/src/main/java/com/example/jakgelapp/MainActivity.java
+++ app/src/main/java/com/example/jakgelapp/MainActivity.java
@@ -4,23 +4,54 @@ import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.nfc.*;
+import android.nfc.tech.Ndef;
 import android.os.Bundle;
 import android.os.Parcelable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
     private NfcAdapter nfcAdapter;
     private TextView nfcContentTextView;
+    private int currentInquery;
+    TextView Saldo, hinText;
+    Tag nfcTag;
+    Button TopUpButton;
+    EditText Ammount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TopUpButton = findViewById(R.id.topBtn);
+        Ammount = findViewById(R.id.ammount);
+        Saldo = findViewById(R.id.Saldo);
+        hinText = findViewById(R.id.hintText);
+
+        TopUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(nfcTag == null || Ammount.getText().equals("")){
+                    System.out.println(nfcTag);
+                }else{
+                    try{
+                        write(Ammount.getText().toString(), nfcTag);
+                    }catch (Error | IOException | FormatException e){
+                        System.out.println(e.getMessage());
+                    }
+                }
+            }
+        });
 
         nfcContentTextView = findViewById(R.id.nfcStatus);
         nfcAdapter = NfcAdapter.getDefaultAdapter(this);
@@ -59,12 +90,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
+        setIntent(intent);
         handleIntent(intent);
+        nfcTag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
     }
 
     private void handleIntent(Intent intent) {
-        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(intent.getAction())) {
+        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(intent.getAction()) ||
+                NfcAdapter.ACTION_NDEF_DISCOVERED.equals(intent.getAction()) ||
+                NfcAdapter.ACTION_TECH_DISCOVERED.equals(intent.getAction()) ) {
             processNdefMessage(intent);
+            nfcTag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
         }
     }
 
@@ -78,7 +114,10 @@ public class MainActivity extends AppCompatActivity {
                         Arrays.equals(record.getType(), NdefRecord.RTD_TEXT)) {
                     try {
                         String text = readTextFromNdefRecord(record);
+                        currentInquery = Integer.parseInt(text);
                         nfcContentTextView.setText("NFC Content: " + text);
+                        Saldo.setText("Rp "+ text);
+                        hinText.setVisibility(View.INVISIBLE);
                     } catch (UnsupportedEncodingException e) {
                         e.printStackTrace();
                     }
@@ -103,5 +142,37 @@ public class MainActivity extends AppCompatActivity {
 
         // Get the Text
         return new String(payload, languageCodeLength + 1, payload.length - languageCodeLength - 1, textEncoding);
+    }
+
+    private void write(String text, Tag tag) throws  IOException, FormatException {
+        currentInquery += Integer.parseInt(text);
+        NdefRecord[] records = {createRecords(String.valueOf(currentInquery))};
+        NdefMessage message = new NdefMessage(records);
+
+        Ndef ndef = Ndef.get(tag);
+        ndef.connect();
+        ndef.writeNdefMessage(message);
+        ndef.close();
+        Saldo.setText(String.valueOf("Rp "+currentInquery));
+        Ammount.setText("");
+    }
+
+    private NdefRecord createRecords(String text) throws UnsupportedEncodingException{
+        String language = "en";
+        byte[] textByte = text.getBytes();
+        byte[] langByte =  language.getBytes(StandardCharsets.UTF_8);
+        int langLength = langByte.length;
+        int textLength = textByte.length;
+        byte[] payload = new byte[1 + langLength + textLength];
+        payload[0] = (byte) langLength;
+
+        System.arraycopy(langByte, 0, payload, 1, langLength);
+        System.arraycopy(textByte, 0, payload, 1+ langLength, textLength);
+
+        return new NdefRecord(NdefRecord.TNF_WELL_KNOWN, NdefRecord.RTD_TEXT, new byte[0], payload);
+    }
+
+    public void WriteText(View view) {
+
     }
 }
